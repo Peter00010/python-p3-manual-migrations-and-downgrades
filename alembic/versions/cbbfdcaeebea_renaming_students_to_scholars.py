d519376f7a195a42260506de7712e2fb42b074b9
Peter00010 <mwongela.oboe@gmail.com>
2024-03-13 15:05:32 +0300

    Done

---
+++ alembic/versions/cbbfdcaeebea_renaming_students_to_scholars.py
@@ -0,0 +1,24 @@
+"""Renaming students to scholars
+
+Revision ID: cbbfdcaeebea
+Revises: 
+Create Date: 2024-03-13 14:31:55.854653
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cbbfdcaeebea'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
